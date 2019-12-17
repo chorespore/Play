e90a4c231db97538f82468cc01b2d7a13af87bef
Zhai Guangchao <593595845@qq.com>
2019-12-17 23:35:26 +0800

    Leet 144A Binary Tree Preorder Traversal

---
+++ src/medium/Leet144A.java
@@ -0,0 +1,33 @@
+package medium;
+
+import main.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Leet144A {
+
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
+}
