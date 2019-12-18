3eec7aa86dfbf3cd7275704a827cf4623c5271fb
Zhai Guangchao <593595845@qq.com>
2019-12-18 23:43:14 +0800

    Leet145A Binary Tree Postorder Traversal

--- src/medium/Leet94A.java
+++ src/medium/Leet94A.java
@@ -1,4 +1,4 @@
-package hard;
+package medium;
 
 import main.TreeNode;
 
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class Leet145A {
+public class Leet94A {
     private final int WHITE = 0;
-    private final int GRAY = 1;
+    private int GREY = 1;
 
     class ColorNode {
         TreeNode node;
@@ -20,7 +20,7 @@ public class Leet145A {
         }
     }
 
-    public List<Integer> postorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<ColorNode> stack = new Stack<>();
 
@@ -29,13 +29,14 @@ public class Leet145A {
         }
 
         stack.push(new ColorNode(root, WHITE));
+
         while (!stack.isEmpty()) {
             ColorNode cn = stack.pop();
             if (cn.color == WHITE) {
-                stack.push(new ColorNode(cn.node, GRAY));
                 if (cn.node.right != null) {
                     stack.push(new ColorNode(cn.node.right, WHITE));
                 }
+                stack.push(new ColorNode(cn.node, GREY));
                 if (cn.node.left != null) {
                     stack.push(new ColorNode(cn.node.left, WHITE));
                 }
@@ -43,7 +44,6 @@ public class Leet145A {
                 res.add(cn.node.val);
             }
         }
-
         return res;
     }
 }
