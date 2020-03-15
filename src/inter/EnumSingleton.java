91bc90f75712fc82b0a6025d7aede6d8357df454
Zhai Guangchao <593595845@qq.com>
2020-03-15 20:45:08 +0800

    Update EnumSingleton

--- src/inter/EnumSingleton.java
+++ src/inter/EnumSingleton.java
@@ -1,5 +1,9 @@
 package inter;
 
 public enum EnumSingleton {
-    INSTANCE
+    INSTANCE;
+
+    public void doSomething() {
+        System.out.println("doSomething");
+    }
 }
--- src/inter/Test.java
+++ src/inter/Test.java
@@ -4,5 +4,8 @@ public class Test {
     public static void main(String[] args) {
         Singleton instance = Singleton.INSTANCE;
         System.out.println(instance);
+
+        EnumSingleton enumSingleton = EnumSingleton.INSTANCE;
+        enumSingleton.doSomething();
     }
 }
